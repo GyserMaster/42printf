ec794d8f0394ef0e6d9e49081f926ff211a82501
Sergey <gyser.world@gmail.com>
2023-04-02 20:21:13 +0200

    Printf v0.02

--- ft_printf.c
+++ ft_printf.c
@@ -11,18 +11,24 @@
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdarg.h>
 #include	"ft_putchar_fd.c"
 #include	"ft_putstr_fd.c"
+#include	"ft_putnbr_fd.c"
+#include	"ft_putdouble_fd.c"
 
 int	ft_check_wildcard(char c, char *buffer)
 {
 	ft_putstr_fd(buffer, 1);
-	
 	return (0);
 }
 
 int	ft_printf(char const *str, ...)
 {
+	va_list valist;
+	int i;
+
+	va_start(valist, 0);
 	while (*str)
 	{
 		if (*str != '%')
@@ -31,26 +37,51 @@ int	ft_printf(char const *str, ...)
 		}
 		else
 		{
-			ft_check_wildcard(*str, "XXX");
+			if (str[1] == '%')
+			{
+				ft_putchar_fd('%', 1);
+			}
+			else if (str[1] == 's')
+			{
+				ft_putstr_fd(va_arg(valist, char*), 1);
+			}
+			else if (str[1] == 'd')
+			{
+				ft_putnbr_fd(va_arg(valist, int), 1);
+			}
+			else if (str[1] == 'c')
+			{
+				ft_putchar_fd(va_arg(valist, int), 1);
+			}
+			else if (str[1] == 'f')
+			{
+				//ft_putdouble_fd(va_arg(valist, double), 1);
+			}
+			else
+			{
+				//ft_putstr_fd(va_arg(valist, char*), 1);
+				ft_putstr_fd("va_arg", 1);
+				
+			}
 			str++;
 		}
-		
 		str++;
 	}
-	
+	va_end(valist);
 	ft_putstr_fd((char*)str, 1);
 	return (0);
 }
 
 int	main(void)
 {
-	char	*dashb = "Name: %s\nAge: %d\nSex: %c";
+	char	*dashb = "Name: %s\nAge: %d\nSex: %c\nScore: %f";
 	char	*name = "spetrov";
 	int		age = 29;
 	char	sex = 'M';
+	double	score = 93.1;
 
 	
-	ft_printf(dashb, name, age, sex);
+	ft_printf("Name: %s\nAge: %d\nSex: %c\nScore: %f", name, age, sex, score);
 	printf("\n>> Hey! Main ft_printf.c");
 	return (0);
 }
